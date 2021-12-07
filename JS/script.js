84aba66d95de529b7bfd2d6e4ef4a38e8e227ea4
El <mattianc@hotmail.it>
2021-12-07 15:52:22 +0100

    aioooo

--- JS/script.js
+++ JS/script.js
@@ -4,14 +4,32 @@ $(document).ready(function(){
 
 $(".container").click(function(){
 	$(this).addClass('active');
-	$(this).children(":first").addClass('hide-ghost'); // show horizontal title
-	$(this).children('.title').addClass('show-title'); //hide vertical title = open card
-
-	
-
-	
+	$(this).children(":first").addClass('hide-ghost'); // hide vertical title
+	$(this).children('.content-section').addClass('show-title'); //show horizontal = open card
 
 	// Collapse others
 	$('.container').not(this).removeClass('active');
 	$('.container').not(this).children().removeClass('show-title hide-ghost'); // show vertical title again and hide horizontal
 })
+
+
+// categories card generation
+data = d3.json("assets/data/img.json");
+
+// Select the container where we will put our HTML elements
+const boxes = d3.select("#categories-box");
+
+// Load data
+data.then(function(data) {
+
+	// We create a single "card". It can be named as you prefer.
+    let box = boxes.selectAll("div") // Inside our selection, select the available div elements
+    .data(data) // Bind the selection to the data
+    .enter() // Create new elements if the selection doesn't match the data
+    .append("div") // Append a new div for each new data point
+    .classed("box flex-display-center-center", true) // Add a class
+
+    box.append("img") // Inside the previous selection, add a new HTML element
+    .classed("box-img", true)
+    .attr("src", function(d) { return d.url }); // Add an attribute to the HTML element. In this case, the src attribute sets the URL of the image we need to display.
+});
