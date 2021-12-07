7044a44b2af835d2b5472a662266b35bf123f7e0
El <mattianc@hotmail.it>
2021-12-07 04:10:53 +0100

    ghost title and title

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-	$('.container > .title').addClass('hide'); // hide horizontal title = collapsed card
-	$('.container').addClass('test');
+	$('.container > .title').addClass('hide-title'); // hide horizontal title = collapsed card
 })
 
 $(".container").click(function(){
 	$(this).addClass('active');
-	$(this).children(":first").addClass('green');
+	$(this).children(":first").addClass('hide-ghost'); // show horizontal title
+	$(this).children('.title').addClass('show-title'); //hide vertical title = open card
+
+	
+
+	
 
 	// Collapse others
 	$('.container').not(this).removeClass('active');
-	$('.container').not(this).children().removeClass('green');
-	// $('.container').not(this).children().addClass('hide');
+	$('.container').not(this).children().removeClass('show-title hide-ghost'); // show vertical title again and hide horizontal
 })
