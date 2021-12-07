cbeab6ef9e9e95ddb7ad9021f237c8a3581dd1c6
El <mattianc@hotmail.it>
2021-12-07 03:42:09 +0100

    aaaiutoo

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,14 @@
-function outputUpdate(vol) {
-	document.querySelector('#volume').value = vol;
-}
+$(document).ready(function(){
+	$('.container > .title').addClass('hide'); // hide horizontal title = collapsed card
+	$('.container').addClass('test');
+})
 
 $(".container").click(function(){
 	$(this).addClass('active');
-	$(this).children().addClass('green');
+	$(this).children(":first").addClass('green');
 
 	// Collapse others
 	$('.container').not(this).removeClass('active');
 	$('.container').not(this).children().removeClass('green');
+	// $('.container').not(this).children().addClass('hide');
 })
